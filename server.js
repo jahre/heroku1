bd3bd0a77f8896c2ac6852997cef3d1d6289dfa3
Ivan Amelyanenka <ivan.amelyanenka@gmail.com>
2019-07-05 00:10:36 +0300

    deploy fix

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const app = express();
 const forceSSL = function() {
     return function (req, res, next) {
       if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
       }
       next();
     }
@@ -21,10 +19,10 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/heroku1'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/heroku1/index.html'));
   });
 
 app.listen(process.env.PORT || 8080);
